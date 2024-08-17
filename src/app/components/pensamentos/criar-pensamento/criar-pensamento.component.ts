8269b59380e408bbe585900259feb4743c1b4560
Jesse das Neves <jesserafael@hotmail.com.br>
2024-08-17 17:56:32 -0300

    inicio

---
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento/pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css'],
+})
+export class CriarPensamentoComponent {
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: 'modelo1',
+  };
+
+  constructor(
+    private readonly service: PensamentoService,
+    private readonly router: Router
+  ) {}
+
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento']);
+    });
+  }
+}
